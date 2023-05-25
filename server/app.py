b5387a6277c7d7d6a2f97f743d7e2569f2d79ce2
Sterling <sterlingbarton81@gmail.com>
2023-05-25 15:07:21 -0600

    completed lab

--- server/app.py
+++ server/app.py
@@ -16,12 +16,53 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(
+            response_dict_list,
+            200
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    def post(self):
+        new_plant = Plant(
+            name=request.get_json()["name"],
+            image=request.get_json()["image"],
+            price=request.get_json()["price"]
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_dict = new_plant.to_dict()
+        response = make_response(
+            response_dict,
+            201
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+api.add_resource(Plants, '/plants')
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+api.add_resource(PlantByID, '/plants/<int:id>')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
